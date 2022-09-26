05e305df942fc9ee5d6fd79d34f0f9851573234b
tamil6300 <tamil632000@gmail.com>
2022-09-26 14:42:11 +0530

    Rasi details

---
+++ app/src/main/java/com/vibame/telugupanchangamcalendar/MonthlyHoroscopeActivity.java
@@ -0,0 +1,120 @@
+package com.vibame.telugupanchangamcalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MonthlyHoroscopeActivity extends AppCompatActivity {
+    RelativeLayout rlc1,rlc2,rlc3,rlc4,rlc5,rlc6,rlc7,rlc8,rlc9,rlc10,rlc12,rlc11;
+    Activity activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_horoscope);
+        activity = MonthlyHoroscopeActivity.this;
+
+
+        rlc1 = findViewById(R.id.rlc1);
+        rlc2 = findViewById(R.id.rlc2);
+        rlc3 = findViewById(R.id.rlc3);
+        rlc4 = findViewById(R.id.rlc4);
+        rlc5 = findViewById(R.id.rlc5);
+        rlc6 = findViewById(R.id.rlc6);
+        rlc7 = findViewById(R.id.rlc7);
+        rlc8 = findViewById(R.id.rlc8);
+        rlc9 = findViewById(R.id.rlc9);
+        rlc10 = findViewById(R.id.rlc10);
+        rlc11 = findViewById(R.id.rlc11);
+        rlc12 = findViewById(R.id.rlc12);
+
+        rlc1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlc11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,RaasiDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/vibame/telugupanchangamcalendar/fragments/HoroscopeFragment.java
+++ app/src/main/java/com/vibame/telugupanchangamcalendar/fragments/HoroscopeFragment.java
@@ -1,13 +1,22 @@
 package com.vibame.telugupanchangamcalendar.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.vibame.telugupanchangamcalendar.DailyHoroscopeActivity;
+import com.vibame.telugupanchangamcalendar.MonthlyHoroscopeActivity;
 import com.vibame.telugupanchangamcalendar.R;
+import com.vibame.telugupanchangamcalendar.WeeklyHoroscopeActivity;
+import com.vibame.telugupanchangamcalendar.YearlyHoroscopeActivity;
 
 public class HoroscopeFragment extends Fragment {
+
+    LinearLayout llDailyhoroscope,llWYearlyHoroscope,llWeeklyHoroscope,llMonthlyHoroscope;
 
 
     public HoroscopeFragment() {
@@ -19,6 +28,46 @@ public class HoroscopeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_horoscope, container, false);
+      View   root = inflater.inflate(R.layout.fragment_horoscope, container, false);
+
+
+        llDailyhoroscope = root.findViewById(R.id.llDailyhoroscope);
+        llMonthlyHoroscope = root.findViewById(R.id.llMonthlyHoroscope);
+        llWYearlyHoroscope = root.findViewById(R.id.llWYearlyHoroscope);
+        llWeeklyHoroscope = root.findViewById(R.id.llWeeklyHoroscope);
+
+        llDailyhoroscope.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DailyHoroscopeActivity.class);
+                startActivity(intent);
+            }
+        });
+        llWeeklyHoroscope.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), WeeklyHoroscopeActivity.class);
+                startActivity(intent);
+            }
+        });
+        llWYearlyHoroscope.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), YearlyHoroscopeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        llMonthlyHoroscope.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MonthlyHoroscopeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+      return root;
+
     }
 }
