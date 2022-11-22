73d1ee147dc155b213a6925e59058d74e9cd6b0f
vishalaravind754 <vishalaravind754@gmail.com>
2022-11-22 22:00:19 +0530

    Smart_Tools_Fragment will be update.

--- app/src/main/java/com/vibame/telugupanchangamcalendar/activities/NumbertoWordsActivity.java
+++ app/src/main/java/com/vibame/telugupanchangamcalendar/activities/NumbertoWordsActivity.java
@@ -1,5 +1,9 @@
 package com.vibame.telugupanchangamcalendar.activities;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -7,6 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,11 +20,12 @@ import com.vibame.telugupanchangamcalendar.R;
 
 public class NumbertoWordsActivity extends AppCompatActivity {
 
-    TextView tvText;
+    TextView tvText, ivCopy;
     EditText edText;
     ImageView imgBack;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +34,23 @@ public class NumbertoWordsActivity extends AppCompatActivity {
 
         tvText = findViewById(R.id.tvText);
         edText = findViewById(R.id.edText);
+        ivCopy = findViewById(R.id.ivCopy);
         imgBack = findViewById(R.id.imgBack);
+
+
+
+        ivCopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("TextView", tvText.getText().toString());
+                clipboardManager.setPrimaryClip(clip);
+
+                Toast.makeText(NumbertoWordsActivity.this, "Copied", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
